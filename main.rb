cc24597c8e07b1750387a2014c397e3d686e8425
Logan Masniuk <logan.masniuk@gmail.com>
2025-07-31 13:44:04 -0700

    Play with strings

--- main.rb
+++ main.rb
@@ -1,1 +1,16 @@
 puts "Hello World!"
+puts("Hello World!") #Allows use to put parentheses around the string
+puts 'Hello World!' #Single quotes also work
+puts %q(Hello World!) #Using %q for single-quoted strings
+
+
+puts "It's Time!"
+
+puts 'Double Quotes "are" allowed'
+
+puts "Single quotes 'are' allowed too"
+
+
+dog_name = "Zeus"
+
+puts dog_name

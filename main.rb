02f64bb17042aaa9baa7da6572345cc4996342ec
Logan Masniuk <logan.masniuk@gmail.com>
2025-08-07 18:17:41 -0700

    Play around with input

--- main.rb
+++ main.rb
@@ -14,3 +14,42 @@ puts "Single quotes 'are' allowed too"
 dog_name = "Zeus"
 
 puts dog_name
+
+dog_name = "Rex"
+puts dog_name
+
+
+a = "Tom"
+b="Hardy"
+
+c = a+b
+
+puts c
+
+a << "Jerry"
+
+puts c
+
+puts a
+
+num1 = 11
+
+
+puts "The number is #{num1}" # interpolation only works with double quotes
+puts 'The number is #{num1}'
+
+
+words = ["Hello", "world"]
+puts words.join   # => "Hello world"
+
+
+
+users_name = gets.chomp
+
+puts "Hello #{users_name}"
+
+puts users_name.length
+
+puts users_name.class
+
+puts users_name.to_i
